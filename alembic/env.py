c59d33e326f12c092379db87f0d7d89c510de9ae
hemanthhari2000 <hemanthhari2000@gmail.com>
2025-02-08 21:52:49 +0530

    Fix. alembic autogenerate issue

--- alembic/env.py
+++ alembic/env.py
@@ -3,8 +3,8 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config, pool
 
 from alembic import context
-from todoforge.db import get_db_url
-from todoforge.schema import Base
+from todoforge.db.schema import Base
+from todoforge.db.util import get_db_url
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
